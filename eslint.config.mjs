a57f0a80f677a630481a3a1510b5a4167444fa67
Timothy Wong <timothywong@TimothydeMacBook-Pro.local>
2024-05-25 03:34:55 +0800

    fix: broken eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,17 +1,46 @@
+import eslint from '@eslint/js'
+import tseslint from 'typescript-eslint'
+import tseslintParser from '@typescript-eslint/parser'
+import simpleImportSort from 'eslint-plugin-simple-import-sort'
+import eslintConfigPrettier from 'eslint-config-prettier'
+
 export default [
+  eslint.configs.recommended,
+  ...tseslint.configs.recommendedTypeChecked,
   {
-    extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'prettier'],
-    plugins: ['@typescript-eslint', 'simple-import-sort', 'prettier'],
-    parser: '@typescript-eslint/parser',
+    languageOptions: {
+      parser: tseslintParser,
+      parserOptions: {
+        project: true,
+        tsconfigRootDir: import.meta.dirname,
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+      },
+    },
+  },
+  eslintConfigPrettier,
+  {
+    plugins: {
+      'simple-import-sort': simpleImportSort,
+    },
     rules: {
       '@typescript-eslint/no-explicit-any': 'off',
+      // Note: you must disable the base rule as it can report incorrect errors
+      'no-unused-vars': 'off',
+      '@typescript-eslint/no-unused-vars': [
+        'error',
+        {
+          args: 'all',
+          argsIgnorePattern: '^_',
+          caughtErrors: 'all',
+          caughtErrorsIgnorePattern: '^_',
+          destructuredArrayIgnorePattern: '^_',
+          varsIgnorePattern: '^_',
+          ignoreRestSiblings: true,
+        },
+      ],
       'simple-import-sort/imports': 'error',
       'simple-import-sort/exports': 'error',
-      'prettier/prettier': 'error',
-    },
-    parserOptions: {
-      ecmaVersion: '6',
-      sourceType: 'module',
     },
   },
 ]
