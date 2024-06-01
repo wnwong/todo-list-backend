9c1dd1dd594c23a1a44e7921d21cd821929b2c56
Timothy Wong <timothywong@TimothydeMacBook-Pro.local>
2024-06-01 15:20:15 +0800

    test: update todo list mock data

--- src/tests/mockData/todo-list.ts
+++ src/tests/mockData/todo-list.ts
@@ -1,31 +1,19 @@
 const mockTodoList = [
   {
     id: 38,
-    name: 'Glory glory man united',
-    createdAt: '2024-05-27T17:48:16.590Z',
-    updatedAt: '2024-05-27T17:48:16.590Z',
-    deletedAt: null,
+    name: 'manchester',
   },
   {
     id: 35,
-    name: 'man city',
-    createdAt: '2024-05-26T18:21:35.097Z',
-    updatedAt: '2024-05-26T18:21:35.097Z',
-    deletedAt: null,
+    name: 'liverpool',
   },
   {
     id: 34,
     name: 'brighton',
-    createdAt: '2024-05-26T18:21:26.953Z',
-    updatedAt: '2024-05-26T18:21:26.953Z',
-    deletedAt: null,
   },
   {
     id: 33,
     name: 'chelsea',
-    createdAt: '2024-05-26T18:21:19.512Z',
-    updatedAt: '2024-05-26T18:21:19.512Z',
-    deletedAt: null,
   },
 ]
 
